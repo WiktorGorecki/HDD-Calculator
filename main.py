a2def2e19e0ec97bf6c7f09fbc9f22eef105ed68
WiktorGórecki <wiktor.gorecki@o2.pl>
2023-06-15 16:36:40 +0200

    Some work on code quality Renamed the functions and variables to use snake_case for consistency. Changed the global constants cyan, green, and grey to uppercase names (CYAN, GREEN, GREY). Removed unnecessary blank lines and added consistent spacing. Replaced the floor function import with integer division (//) to calculate number_of_blocks. Updated the recursive call to show_stats_menu to use consistent function naming. Changed the while 1 loops to while True loops for readability. Improved the formatting of multi-line print statements. Fixed a typo in the comment of calculate_fitting_blocks function. Updated the main function call to use the renamed functions.
    Signed-off-by: WiktorGórecki <wiktor.gorecki@o2.pl>

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
-# TODO: Make a way to manually insert data (with only check if it makes sense (is efficient and will work))
-# TODO: Move blanks from functions to place before it's call
-from math import floor
-
 import os
 
 os.system("")
 
-cyan = '\033[36m'
-green = '\033[32m'
-grey = '\033[37m'
+CYAN = '\033[36m'
+GREEN = '\033[32m'
+GREY = '\033[37m'
 
 
-def stats_menu(number_of_blocks, word_width, disk_space, address_width):
+def show_stats_menu(number_of_blocks, word_width, disk_space, address_width):
     answer = input("> ")
     if answer == "1":
         clear_screen()
-        logo()
+        show_logo()
         run()
     elif answer == "2":
-        print_disk(number_of_blocks, word_width, disk_space, address_width)
+        show_disk(number_of_blocks, word_width, disk_space, address_width)
     elif answer == "3":
         quit(0)
     else:
-        stats_menu(number_of_blocks, word_width, disk_space, address_width)
+        show_stats_menu(number_of_blocks, word_width, disk_space, address_width)
 
 
-def stats(number_of_blocks: int, word_width: int, disk_space: int, address_width: int):
+def show_stats(number_of_blocks: int, word_width: int, disk_space: int, address_width: int):
     blanks = disk_space - ((word_width * number_of_blocks) + (address_width * number_of_blocks))
 
     # Printing the stats
@@ -38,42 +34,42 @@ def stats(number_of_blocks: int, word_width: int, disk_space: int, address_width
 
     # Menu
     print("\n\n[1] Generate new drive")
-    print("[2] Visualise the drive")
+    print("[2] Visualize the drive")
     print("[3] Exit")
-    stats_menu(number_of_blocks, word_width, disk_space, address_width)
+    show_stats_menu(number_of_blocks, word_width, disk_space, address_width)
 
 
-def print_disk(number_of_blocks: int, word_width: int, disk_space: int, address_width: int, prefix=""):
+def show_disk(number_of_blocks: int, word_width: int, disk_space: int, address_width: int, prefix=""):
     counter = 0
     blanks = disk_space - ((word_width * number_of_blocks) + (address_width * number_of_blocks))
     print("\n Drive model:\n")
     print(prefix, end="")
-    for p in range(0, len(prefix)):
+    for p in range(len(prefix)):
         blanks -= 1
-    for b in range(0, number_of_blocks):
-        for a in range(0, address_width):
-            print(cyan + "a", end="")
+    for b in range(number_of_blocks):
+        for a in range(address_width):
+            print(CYAN + "a", end="")
             counter += 1
-        for w in range(0, word_width):
-            print(green + "w", end="")
+        for w in range(word_width):
+            print(GREEN + "w", end="")
             counter += 1
-    for blank in range(0, blanks):
-        print(grey + "u", end="")
+    for _ in range(blanks):
+        print(GREY + "u", end="")
         counter += 1
     if counter != disk_space:
         if counter > disk_space:
-            print("Error: The disk visualisation has more bits than the actual disk!!!")
+            print("Error: The disk visualization has more bits than the actual disk!!!")
         elif counter < disk_space:
-            print("Error: The disk visualisation has less bits than the actual disk!!!")
-            print("You should count the drawn bits and check if it matches number of address and word bits. If yes, "
-                  "then you should probably add some blanks, and call it a day")
+            print("Error: The disk visualization has fewer bits than the actual disk!!!")
+            print("You should count the drawn bits and check if it matches the number of address and word bits. "
+                  "If yes, then you should probably add some blanks and call it a day")
         else:
-            print("Error: The disk visualisation doesn't contain valid number of bits!!!")
+            print("Error: The disk visualization doesn't contain a valid number of bits!!!")
 
-    # Legend informing what does each colour represent
-    print(cyan + "\n\naddress")
-    print(green + "word")
-    print(grey + "unassigned")
+    # Legend informing what each color represents
+    print(CYAN + "\n\naddress")
+    print(GREEN + "word")
+    print(GREY + "unassigned")
     print('\x1b[0m')
 
     # Menu
@@ -83,10 +79,10 @@ def print_disk(number_of_blocks: int, word_width: int, disk_space: int, address_
     answer = input("> ")
     if answer == "1":
         clear_screen()
-        logo()
+        show_logo()
         run()
     elif answer == "2":
-        stats(number_of_blocks, word_width, disk_space, address_width)
+        show_stats(number_of_blocks, word_width, disk_space, address_width)
     elif answer == "3":
         quit(0)
 
@@ -95,16 +91,16 @@ def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-def logo():
+def show_logo():
     """
-    Function that prints name of the program and name of the author.
+    Function that prints the name of the program and the name of the author.
     """
     print("Simple HDD calculator")
     print("By: Wiktor Górecki (https://github.com/WiktorGorecki)")
 
 
-def number_input(msg: str):
-    while 1:
+def get_number_input(msg: str):
+    while True:
         try:
             result = int(input(msg))
             break
@@ -120,51 +116,51 @@ def drive_input(error_message=""):
         print()
     print()
     print("Specify drive properties:")
-    disk_space = number_input("    Capacity :     ")
-    word_width = number_input("    Size of word:  ")
+    disk_space = get_number_input("    Capacity :     ")
+    word_width = get_number_input("    Size of word:  ")
     return disk_space, word_width
 
 
 def run(error_message=""):
     disk_space, word_width = drive_input(error_message)
-    number_of_blocks = floor(disk_space / word_width)
+    number_of_blocks = disk_space // word_width
 
-    calculated_number_of_blocks, calculated_address_width = calculate_number_of_fitting_blocks(number_of_blocks,
-                                                                                               disk_space, word_width)
+    calculated_number_of_blocks, calculated_address_width = calculate_fitting_blocks(number_of_blocks,
+                                                                                     disk_space, word_width)
     print("\nCalculated parameters:")
     print("    Number of words: " + str(calculated_number_of_blocks))
     print("    Address width:  " + str(calculated_address_width))
 
     print("\n\n[1] Generate new drive")
-    print("[2] Visualise the drive")
+    print("[2] Visualize the drive")
     print("[3] Drive statistics")
     print("[4] Exit")
     answer = input("> ")
     if answer == "1":
         run()
     elif answer == "2":
-        print_disk(calculated_number_of_blocks, word_width, disk_space, calculated_address_width)
+        show_disk(calculated_number_of_blocks, word_width, disk_space, calculated_address_width)
     elif answer == "3":
-        stats(calculated_number_of_blocks, word_width, disk_space, calculated_address_width, )
+        show_stats(calculated_number_of_blocks, word_width, disk_space, calculated_address_width)
     elif answer == "4":
         quit(0)
 
 
-def calculate_number_of_fitting_blocks(number_of_blocks: int, disk_space: int, word_width: int):
+def calculate_fitting_blocks(number_of_blocks: int, disk_space: int, word_width: int):
     """
-    Calculates maximum number of blocks that fit on specified drive
-    :param number_of_blocks: Number of blocks of data that the disk will store
-    :param disk_space: Number of unassigned bits that the program will assign
-    :param word_width: Number of bits that one block will store
-    :return: Number of words, Width of the address in a block
+    Calculates the maximum number of blocks that fit on the specified drive.
+    :param number_of_blocks: Number of blocks of data that the disk will store.
+    :param disk_space: Number of unassigned bits that the program will assign.
+    :param word_width: Number of bits that one block will store.
+    :return: Number of words, Width of the address in a block.
     """
     address_width = 1
-    while 1:
+    while True:
         max_address = address_width ** 2
         if max_address < number_of_blocks:
             address_width += 1
 
-        # Check if disk will fit all the data
+        # Check if the disk will fit all the data
         used_bits = (address_width * number_of_blocks) + (word_width * number_of_blocks)
         while used_bits > disk_space:
             number_of_blocks -= 1
@@ -174,5 +170,5 @@ def calculate_number_of_fitting_blocks(number_of_blocks: int, disk_space: int, w
             return number_of_blocks, address_width
 
 
-logo()
+show_logo()
 run()
